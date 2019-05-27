8d05113215c5b4f8aa5239208b9d16fcce3ffe3b
Umputun <umputun@gmail.com>
2019-05-27 14:43:48 -0500

    retry agent dial on failed syslog

--- app/agent/syslog/syslog_unix.go
+++ app/agent/syslog/syslog_unix.go
@@ -3,13 +3,21 @@
 package syslog
 
 import (
+	"context"
 	"io"
 	"log/syslog"
+	"time"
+
+	"github.com/go-pkgz/repeater"
 )
 
 // GetWriter returns syslog writer for non-win platform
-func GetWriter(syslogHost, syslogPrefix, containerName string) (io.WriteCloser, error) {
-	return syslog.Dial("udp4", syslogHost, syslog.LOG_WARNING|syslog.LOG_DAEMON, syslogPrefix+containerName)
+func GetWriter(syslogHost, syslogPrefix, containerName string) (res io.WriteCloser, err error) {
+	e := repeater.NewDefault(10, time.Second).Do(context.Background(), func() error {
+		res, err = syslog.Dial("udp4", syslogHost, syslog.LOG_WARNING|syslog.LOG_DAEMON, syslogPrefix+containerName)
+		return err
+	})
+	return res, e
 }
 
 // IsSupported indicates if the platform supports syslog

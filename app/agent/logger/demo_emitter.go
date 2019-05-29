7e43bcfe844d52d376840feaa35f198f651acca3
Umputun <umputun@gmail.com>
2019-05-28 23:29:47 -0500

    lint: unhandled error

--- app/agent/logger/demo_emitter.go
+++ app/agent/logger/demo_emitter.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	docker "github.com/fsouza/go-dockerclient"
@@ -20,7 +21,9 @@ func (d *DemoEmitter) Logs(o docker.LogsOptions) error {
 		case <-o.Context.Done():
 			return o.Context.Err()
 		case <-time.After(d.Duration):
-			_, _ = o.OutputStream.Write([]byte(fmt.Sprintf("demo message %d\n", n)))
+			if _, err := o.OutputStream.Write([]byte(fmt.Sprintf("demo message %d\n", n))); err != nil {
+				log.Printf("[WARN] demo log failed, %v", err)
+			}
 			n++
 		}
 	}

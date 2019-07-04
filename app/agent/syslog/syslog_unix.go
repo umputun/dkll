b4479e6fcf3b63dc5715d9520ce60254c9f49bb5
Umputun <umputun@gmail.com>
2019-07-04 16:32:14 -0500

    try TCP dial to syslog if UDP failed

--- app/agent/syslog/syslog_unix.go
+++ app/agent/syslog/syslog_unix.go
@@ -13,10 +13,19 @@ import (
 
 // GetWriter returns syslog writer for non-win platform
 func GetWriter(ctx context.Context, syslogHost, syslogPrefix, containerName string) (res io.WriteCloser, err error) {
+	// try UDP syslog first
 	e := repeater.NewDefault(10, time.Second).Do(ctx, func() error {
 		res, err = syslog.Dial("udp4", syslogHost, syslog.LOG_WARNING|syslog.LOG_DAEMON, syslogPrefix+containerName)
 		return err
 	})
+
+	// try TCP if UDP failed
+	if e != nil {
+		e = repeater.NewDefault(10, time.Second).Do(ctx, func() error {
+			res, err = syslog.Dial("tcp4", syslogHost, syslog.LOG_WARNING|syslog.LOG_DAEMON, syslogPrefix+containerName)
+			return err
+		})
+	}
 	return res, e
 }
 
